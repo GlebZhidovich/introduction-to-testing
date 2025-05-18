e1159d0ab3890c619aa3b397137051b814c34a60
GlebZhidovich <gzhidovich@gmail.com>
2025-05-18 19:10:20 +0300

    feat: add Character object tests

--- examples/characters/src/character.test.ts
+++ examples/characters/src/character.test.ts
@@ -1,14 +1,29 @@
 import { describe, it, expect } from 'vitest';
 import { Character } from './character.js';
-import { Person } from './person.js';
 
 describe('Character', () => {
-  it.todo(
-    'should create a character with a first name, last name, and role',
-    () => {},
-  );
+  const firstName = 'Gleb';
+  const lastName = 'Super';
+  const role = 'Rogue';
+  const character = new Character(firstName, lastName, role);
 
-  it.todo('should allow you to increase the level', () => {});
+  it('should create a character with a first name, last name, and role', () => {
+    expect(character).toHaveProperty('firstName', firstName);
+    expect(character).toHaveProperty('lastName', lastName);
+    expect(character).toHaveProperty('role', role);
+  });
 
-  it.todo('should update the last modified date when leveling up', () => {});
+  it('should allow you to increase the level', () => {
+    const level = character.level + 1;
+    character.levelUp();
+
+    expect(character).toHaveProperty('level', level);
+  });
+
+  it('should update the last modified date when leveling up', () => {
+    const date = character.lastModified;
+    character.levelUp();
+
+    expect(character.lastModified).not.toBe(date);
+  });
 });
